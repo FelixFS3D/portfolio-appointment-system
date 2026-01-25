c00abfe1c9982ace7ed5ecc1efb89e5cb72634fe
FelixFS3D <felix.fs3d@gmail.com>
2026-01-25 11:50:54 +0000

    feat: initial project structure and secure configuration

---
+++ src/test/java/dev/romero/portfolio_api/PortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.romero.portfolio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
